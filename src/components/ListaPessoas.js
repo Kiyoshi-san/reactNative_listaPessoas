f66c33dd78ade80b5513d8a199df1e95fa424bbe
kiyo_yodo <kiyoshibudokan@hotmail.com>
2018-07-24 01:08:56 -0300

    Adicionado Scroll com ScrollView e com FlatList

--- src/components/ListaPessoas.js
+++ src/components/ListaPessoas.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { StyleSheet, View, Text } from "react-native";
+import { StyleSheet, FlatList, Text } from "react-native";
 
 import ListaPessoasItem from "./ListaPessoasItem";
 
@@ -8,24 +8,40 @@ const ListaPessoas = props => {
 	/*const nomeElementos = pessoas.map(pessoas => {
 		const { first } = pessoas.name;
 		return (
-			<View key={ first } style={estilo.linha}>
+			<FlatList key={ first } style={estilo.linha}>
 				<Text style={estilo.linhaText}>{ first }</Text>
-			</View>
 		);
 	});*/
 
-	const itens = pessoas.map(pessoas => {
+
+	// ALTERADO PARA O "FlatList"
+	/*const itens = pessoas.map(pessoas => {
 		return <ListaPessoasItem 
 			key={pessoas.name.first} 
 			pessoas={pessoas} 
 			irParaDetalhes={clicouItem}
 		/>
-	})
+	})*/
 
 	return (
-		<View style={estilo.container}>
+		/*<View style={estilo.container}>
 			{ itens }
-		</View>
+		</View>*/
+		/*
+			HÁ 2 PROPS QUE DEVERÃO SER PASSADOS NO FLATLIST
+			- DATA - ESPERA UM ARRAY DE ITENS
+			- RENDERITEM - É CHAMADO QUANDO ESTA SENDO FEITO O SCROLL PARA QUE RENDERIZE MAIS ITEMS - CHAMANDO A FUNÇÃO QUE ESTÁ SENDO INSTANCIADA PELA "renderItem"
+		*/
+		<FlatList
+			style={estilo.container} 
+			data={pessoas}
+			renderItem={({ item }) => (
+				<ListaPessoasItem 
+					pessoas={item} 
+					irParaDetalhes={clicouItem}
+				/>
+			)}
+		/>
 	)
 };
 
